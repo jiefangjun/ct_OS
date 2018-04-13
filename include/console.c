af1cd93a693b1df5c13c379ccde5ebcdfb483fca
fokia <fokia@qq.com>
2018-04-13 23:04:36 +0800

    output color string in word model

---
+++ include/console.c
@@ -0,0 +1,122 @@
+#include "console.h"
+/*
+ * VGA(Video Graphics Array，视频图形阵列)是使用模拟信号的一种视频传输标准，内核可以通过它来控制屏幕上字符或者图形的显示。
+ * 在默认的文本模式(Text-Mode)下，VGA控制器保留了一块内存(0x8b000~0x8bfa0)作为屏幕上字符显示的缓冲区，
+ * 若要改变屏幕上字符的显示，只需要修改这块内存就好了。
+ *
+ */
+
+// VGA 的显示缓冲的起点是 0xB8000
+static uint16_t *video_memory = (uint16_t *)0xB8000;
+
+static uint8_t cursor_x = 0;
+static uint8_t cursor_y = 0;
+
+static void move_cursor()
+{
+	//屏幕是80字节宽
+	uint16_t cursorLocation = cursor_y * 80 + cursor_x;
+	
+	// VGA 内部的寄存器多达300多个，显然无法一一映射到I/O端口的地址空间。
+	// 对此 VGA 控制器的解决方案是，将一个端口作为内部寄存器的索引：0x3D4，
+	// 再通过 0x3D5 端口来设置相应寄存器的值。
+	// 在这里用到的两个内部寄存器的编号为14与15，分别表示光标位置的高8位与低8位。
+
+	outb(0x3D4, 14);                  	// 告诉 VGA 我们要设置光标的高字节
+	outb(0x3D5, cursorLocation >> 8); 	// 发送高 8 位
+	outb(0x3D4, 15);                  	// 告诉 VGA 我们要设置光标的低字节
+	outb(0x3D5, cursorLocation);     	// 发送低 8 位
+}
+
+// 屏幕滚动操作
+static void scroll()
+{
+	// attribute_byte 被构造出一个黑底白字的描述格式
+	uint8_t attribute_byte = (0 << 4) | (15 & 0x0F);
+	uint16_t blank = 0x20 | (attribute_byte << 8); // space 是 0x20
+
+	// cursor_y 到 25的时候，就该换行了
+	if(cursor_y >= 25)
+       	{
+		// 将所有行的显示数据复制到上一行，第一行永远消失
+		int i;
+		for(i = 0 * 80; i < 24 * 80; i++) {
+			video_memory[i] = video_memory[i+80];
+		}
+
+		// 最后一行数据现在填充空格，不显示任何字符
+		for(i = 24 * 80; i < 25 * 80; i++) {
+			video_memory[i] = blank;
+		}
+
+		// 向上移动了一行，所以cursor_y 现在是24
+		cursor_y = 24;
+	}
+}
+
+// 清屏操作
+void console_clear()
+{
+	uint8_t attribute_byte = (0 << 4) | (15 & 0x0F);
+	uint16_t blank = 0x20 | (attribute_byte << 8);
+
+	int i;
+	for(i = 0; i < 80 * 25; i++) {
+		video_memory[i] = blank;
+	}
+
+	cursor_x = 0;
+	cursor_y = 0;
+	move_cursor();
+}
+
+void console_putc_color(char c, real_color_t back, real_color_t fore)
+{
+	uint8_t back_color = (uint8_t)back;
+	uint8_t fore_color = (uint8_t)fore;
+
+	uint8_t attribute_byte = (back_color << 4) | (fore_color & 0x0F);
+	uint16_t attribute = attribute_byte << 8;
+
+	// 0x08是退格键的ASCII码
+	// 0x09是tab键的ASCII码
+	if(c == 0x08 && cursor_x) {
+		cursor_x--;
+	} else if(c == 0x09) {
+		cursor_x = (cursor_x + 8) & ~(8-1);
+	} else if(c == '\r') {
+		cursor_x = 0;
+		cursor_y++;
+	} else if(c >= ' ') {
+		video_memory[cursor_y * 80 + cursor_x] = c | attribute;
+		cursor_x++;
+	}
+
+	//每80个字符一行，满80就必须换行
+	if(cursor_x >= 80) {
+		cursor_x = 0;
+		cursor_y++;
+	}
+
+	// 如果需要的话滚动屏幕显示
+	scroll();
+
+	//移动硬件的输入光标
+	move_cursor();
+}
+
+void console_write(char *cstr)
+{
+	while (*cstr) {
+		console_putc_color(*cstr++, rc_black, rc_white);
+	}
+}
+
+void console_write_color(char *cstr, real_color_t back, real_color_t fore)
+{
+	while(*cstr) {
+		console_putc_color(*cstr++, back, fore);
+	}
+}
+
+
--- init/entry.c
+++ init/entry.c
@@ -1,21 +1,9 @@
 #include "types.h"
-int kern_entry()
-{
-	uint8_t *input = (uint8_t *)0xB8000;
-    	uint8_t color = (0 << 4) | (15 & 0x0F);
+#include "console.h"
 
-    	*input++ = 'H'; *input++ = color;
-    	*input++ = 'e'; *input++ = color;
-    	*input++ = 'l'; *input++ = color;
-    	*input++ = 'l'; *input++ = color;
-    	*input++ = 'o'; *input++ = color;
-    	*input++ = ','; *input++ = color;
-    	*input++ = ' '; *input++ = color;
-    	*input++ = 'c'; *input++ = color;
-    	*input++ = 't'; *input++ = color;
-    	*input++ = '_'; *input++ = color;
-    	*input++ = 'O'; *input++ = color;
-    	*input++ = 'S'; *input++ = color;
-    	*input++ = '!'; *input++ = color;
+int kern_entry()
+{	
+	console_clear();
+	console_write_color("Hello, ctOS kernel!\n", rc_black, rc_green);
 	return 0;
 }
