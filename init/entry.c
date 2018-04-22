227a982fe77148c75f454cda200dc0fe69dd41d7
fokia <fokia@qq.com>
2018-04-22 17:26:05 +0800

    finish idt

--- init/entry.c
+++ init/entry.c
@@ -6,12 +6,16 @@ int kern_entry()
 {	
 	init_debug();
 	init_gdt();
+	init_idt();
 
 	console_clear();
 	
 	printk_color(rc_black, rc_green, "Hello, ct_OS kernel!\n");
 
-	panic("test");
+	//panic("test");
+
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
 
 	return 0;
 }
